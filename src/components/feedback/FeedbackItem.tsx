cd023c0354e79ca6d224b54c4518605ae0841e15
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-14 21:07:50 +0200

    :star: feat: toggle open/close FeedbackItem

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,13 +1,19 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { FeedbackItemType } from "../../lib/types";
+import { useState } from "react";
 
 type FeedbackItemProps = {
   feedbackItem: FeedbackItemType;
 };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
+  const [open, setOpen] = useState(false);
+
   return (
-    <li className="feedback">
+    <li
+      className={`feedback ${open ? "feedback--expand" : ""}`}
+      onClick={() => setOpen((prev) => !prev)}
+    >
       <button>
         <TriangleUpIcon />
         <span>{feedbackItem.upvoteCount}</span>
